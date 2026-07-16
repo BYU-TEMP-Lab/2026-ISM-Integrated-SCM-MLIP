0f09c69e7bba1a8987986b7d6f209215d0c697e3
IsaacRunner <ihwalker74@gmail.com>
2026-07-16 15:18:05 -0400

    Update submit_cp.sh

--- Shell_Scripts/submit_cp.sh
+++ Shell_Scripts/submit_cp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Calc_Cp
 #SBATCH --output=slurm_Cp_%j.out
-#SBATCH --time=28:00:00
+#SBATCH --time=08:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
