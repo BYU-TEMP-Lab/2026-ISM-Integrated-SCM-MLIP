c49f8808b7bbf229d066e530771a9ce4f216ac78
IsaacRunner <ihwalker74@gmail.com>
2026-07-16 15:41:23 -0400

    Reduce job time limit from 10h to 2h

--- Shell_Scripts/submit_scl.sh
+++ Shell_Scripts/submit_scl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Calc_SCL
 #SBATCH --output=slurm_scl_%j.out
-#SBATCH --time=10:00:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
